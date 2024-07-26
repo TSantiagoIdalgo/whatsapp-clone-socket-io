82db7c020a3a7f65236176b08a1a0e5e9108582f
tomasidalgo51 <tomasidalgo51@gmail.com>
2024-07-26 12:42:43 +0000

    databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { ChatGateway } from './chat/chat.gateway';
 import { UsersModule } from './users/users.module';
 import { ChatModule } from './chat/chat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from './config/db.config';
 
 @Module({
-  imports: [UsersModule, ChatModule, ChatGateway],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      database: DB_NAME,
+      password: DB_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    UsersModule, ChatModule, ChatGateway],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.use(cors({ origin: '*' }))
   await app.listen(3000);
 }
 bootstrap();
